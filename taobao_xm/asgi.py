1694b55475e572f33bbea543cbcb4748e274110d
wz814717288 <814717288@qq.com>
2021-08-27 09:36:50 +0800

    Initial commit

---
+++ taobao_xm/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for taobao_xm project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taobao_xm.settings')
+
+application = get_asgi_application()
